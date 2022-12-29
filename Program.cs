1d0f7ecd500936259a26acdf5effb9b379a0c16e
avvakumov <avvakumov@tslog.ru>
2022-12-29 17:47:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Application_9._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Задание #1   
+            var exceptions = new Exception[] { new ArgumentOutOfRangeException(), new IndexOutOfRangeException(), new MyException(), new NotImplementedException(), new RankException() };
+            foreach (var item in exceptions)
+            {
+                try
+                {
+                    throw item;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            /// Задание #2
+
+
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+}
